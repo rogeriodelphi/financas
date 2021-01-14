4441b88e5b114325934ac242ad6d52932847ec28
rogeriodelphi <rogeriodelphi@gmail.com>
2021-01-14 16:34:25 -0300

    Atualizando a diretiva ALLOWED_HOSTS no settings.py

--- financas/settings.py
+++ financas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dc232t(ms736b6x!@zv@lu5mprgfn0w%dsm!#dk#e04bkf^x^1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
