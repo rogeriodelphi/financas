46412e33c138f54b41efccc5c226b9695812b7e1
rogeriodelphi <rogeriodelphi@gmail.com>
2021-01-14 14:28:37 -0300

    Criando os arquivos de deploy no Heroku

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\rogerio.martins\pycharmprojects\financas\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
