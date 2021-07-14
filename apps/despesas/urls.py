404539b8b61c2813a903ba8951472706724e36fd
rogeriodelphi <rogeriodelphi@gmail.com>
2021-07-14 00:47:23 -0300

    Criada a urls, views e template de editar despesas

--- apps/despesas/urls.py
+++ apps/despesas/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from .views import (
     listar_despesas,
     inserir_despesa,
+    editar_despesa,
 )
 
-app_name = 'apps.despesas'
+app_name = 'despesas'
 
 urlpatterns = [
     path('', listar_despesas, name='listar_despesas'),
     path('inserir', inserir_despesa, name='inserir_despesa'),
+    path('editar/<int:id>/', editar_despesa, name = 'editar_despesa'),
 ]
--- apps/despesas/views.py
+++ apps/despesas/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-from .models import Despesa
 from .forms import DespesaForm
+from .models import Despesa
+
 
 @login_required()
 def listar_despesas(request):
@@ -11,14 +12,27 @@ def listar_despesas(request):
     context = {'despesas': despesas}
     return render(request, template_name, context)
 
+
 @login_required()
 def inserir_despesa(request):
-    template_name = 'despesas/inserir_despesas.html'
+    template_name = 'despesas/inserir_despesa.html'
     if request.method == 'POST':
         form = DespesaForm(request.POST)
         if form.is_valid():
             form.save()
         return redirect('despesas:listar_despesas')
     form = DespesaForm()
+    context = {'form': form}
+    return render(request, template_name, context)
+
+
+@login_required()
+def editar_despesa(request, id):
+    template_name = 'despesas/editar_despesa.html'
+    despesa = Despesa.objects.get(id=id)
+    form = DespesaForm(request.POST or None, instance=despesa)
+    if form.is_valid():
+        form.save()
+        return redirect('despesas:listar_despesas')
     context = {'form': form}
     return render(request, template_name, context)
