297161aebdc06b3d37a7ce0a51da216c6c744eae
rogeriodelphi <rogeriodelphi@gmail.com>
2021-07-17 23:31:01 -0300

    Atualizando todos os templates de inserção da aplicação

--- apps/despesas/forms.py
+++ apps/despesas/forms.py
@@ -4,8 +4,8 @@ from .models import Despesa
 
 
 class DespesaForm(forms.ModelForm):
-    des_itensdespesa = forms.CharField(label='Observações', required=False, widget=forms.Textarea(attrs={'rows': '3'}))
-    des_obs = forms.CharField(label='Observações', required=False, widget=forms.Textarea(attrs={'rows': '3'}))
+    des_itensdespesa = forms.CharField(label='Itens da Despesa', required=False, widget=forms.Textarea(attrs={'rows': '2'}))
+    des_obs = forms.CharField(label='Observações', required=False, widget=forms.Textarea(attrs={'rows': '2'}))
     class Meta:
         model = Despesa
         fields = ['des_datadespesa', 'des_valordespesa', 'nat_despesa', 'fornecedor', 'meiodepagamento', 'des_status', 'des_itensdespesa', 'des_obs']
